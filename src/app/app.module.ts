3ae4bb8c592de29a8fbbcecbf4f9cba9e0a728e7
viettu2k <doanviettu2k@gmail.com>
2022-02-07 11:02:00 +0700

    importing and exporting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ElementsModule } from './elements/elements.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
