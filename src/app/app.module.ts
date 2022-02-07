4eae3c045f5793fae84b4278a2e91c9c675b127f
viettu2k <doanviettu2k@gmail.com>
2022-02-07 11:08:49 +0700

    collections home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CollectionsModule } from './collections/collections.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
