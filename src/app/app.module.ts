e0bca0ade8a20d3f55cca487844c77637ae481ed
viettu2k <doanviettu2k@gmail.com>
2022-02-08 20:52:13 +0700

    adding home and notfound route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
   providers: [],
   bootstrap: [AppComponent],
